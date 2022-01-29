5eed758240427eae716f9e0deacf6db43d366d9d
aleksandarjockovic226 <aleksandarjockovic226@gmail.com>
2022-01-29 22:51:41 +0100

    changed content file layout, and added translation script

--- js/script.js
+++ js/script.js
@@ -15,6 +15,14 @@ function setLang() {
     translate(newLang);
 }
 
-function translate() {
-    console.log(currentLang);
+async function translate() {
+    const response = await fetch('http://localhost:5500/data/content.json')
+    const data = await response.json();
+
+    htmlelement.querySelectorAll('[data-key]').forEach(element => {
+        let key = element.getAttribute('data-key')
+        if (key) {
+            element.textContent = data[currentLang][key]
+        }
+    })
 }
