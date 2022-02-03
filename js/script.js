6cfce1523099f7bf536a77d32fc936dca4c219fc
aleksandarjockovic226 <aleksandarjockovic226@gmail.com>
2022-02-03 21:11:45 +0100

    Styled languages btns. Added toggle modal form.

--- js/script.js
+++ js/script.js
@@ -1,35 +1,54 @@
-document.addEventListener('DOMContentLoaded', function () {
 
-    var ls = localStorage.getItem("lang")
+var ls = localStorage.getItem("lang")
 
-    var htmlelement = document.querySelector('html')
-    var currentLang = htmlelement.getAttribute('lang')
+var htmlelement = document.querySelector('html')
+var currentLang = htmlelement.getAttribute('lang')
 
-    ls !== null ? currentLang = ls : '';
-    translate()
 
-    var languages = document.querySelectorAll('.language')
+
+ls !== null ? currentLang = ls : '';
+translate()
+
+var languages = document.querySelectorAll('.language')
+languages.forEach(item => {
+    item.getAttribute('value') == currentLang ? item.classList.add('current') : '';
+    item.addEventListener('click', () => setLang(item))
+});
+
+function setLang(item) {
     languages.forEach(item => {
-        item.addEventListener('click', setLang)
-    });
+        item.classList.remove('current')
+    })
+    item.classList.toggle('current')
 
-    function setLang() {
-        var newLang = this.getAttribute('value');
-        if (currentLang === newLang) { return };
+    var newLang = item.getAttribute('value');
+    if (currentLang === newLang) { return };
 
-        htmlelement.setAttribute('lang', newLang);
-        currentLang = newLang
-        localStorage.setItem("lang", newLang);
-        translate(newLang);
-    }
+    htmlelement.setAttribute('lang', newLang);
+    currentLang = newLang
+    localStorage.setItem("lang", newLang);
+    translate(newLang);
+}
 
-    async function translate() {
-        const response = await fetch('http://localhost:5500/data/content.json')
-        const data = await response.json();
+async function translate() {
+    const response = await fetch('http://localhost:5500/data/content.json')
+    const data = await response.json();
 
-        htmlelement.querySelectorAll('[data-key]').forEach(element => {
-            let key = element.getAttribute('data-key')
-            element.textContent = data[currentLang][key]
-        })
-    }
+    htmlelement.querySelectorAll('[data-key]').forEach(element => {
+        let key = element.getAttribute('data-key')
+        element.textContent = data[currentLang][key]
+    })
+}
+
+
+var toggleFormBtn = document.querySelectorAll('.toggleForm')
+
+toggleFormBtn.forEach(item => {
+    item.addEventListener('click', formToggle)
 })
+
+function formToggle() {
+    document.querySelector('.modal').classList.toggle('hidden')
+}
+
+
